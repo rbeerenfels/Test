4b4a7fe39db72013101c54706ab2b7acd2bc9a13
https://github.com/rbeerenfels <rbeerenfels@gmail.com>
2014-08-02 18:57:17 +1000

    initialising hello world

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		SecondGreeting message = new SecondGreeting();
+		
+		displayGreeting();
+		message.display();
+		
+	}
+	private static void displayGreeting() {
+		System.out.println("HELLO WORLD!!");
+	}
+	
+	
+
+}
---
+++ HelloWorld/src/SecondGreeting.java
@@ -0,0 +1,9 @@
+
+public class SecondGreeting {
+
+	public void display() {
+		System.out.println("Hello World :)");
+		
+	}
+
+}
